2560f031a69ffdc0c163bef5885369ce3776d88b
Sergei Prutsakov <psn206@mail.ru>
2024-07-03 17:25:40 +0300

    Добавьте файлы проекта.

---
+++ MVVM/Models/CalcGeometry.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVM.Models
+{
+   static class CalcGeometry
+    { 
+       public static double Сircumference(double r) => 2 * r * Math.PI;
+    }
+}
---
+++ MVVM/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,68 @@
+﻿using MVVM.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace MVVM.ViewModels
+{
+    internal class MainWindowViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        double input;
+        double output;
+        public double Input
+        {
+            get => input;
+            set
+            {
+                input = value;
+                OnPropertyChenged();
+            }
+        }
+
+        public double Output
+        {
+            get => output;
+            set
+            {
+                output = value;
+                OnPropertyChenged();
+            }
+        }
+
+        public MainWindowViewModel()
+        {
+            GetСircumference = new RelayCommand(OnСircumferenceCommandExecut, CanСircumferenceCommandExecut);
+        }
+
+        void OnPropertyChenged([CallerMemberName] string PropertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName));
+        }
+
+        public ICommand GetСircumference { get; }
+        private void OnСircumferenceCommandExecut(Object p)
+        {
+            Output = CalcGeometry.Сircumference(Input);
+        }
+        private bool CanСircumferenceCommandExecut(Object p)
+        {
+            if (Input != 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }            
+        }
+      
+    }
+}
